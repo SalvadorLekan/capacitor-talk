6622611b9d653fe49d25aada359adff4b099aa35
SalvadorLekan <lekansalvador@gmail.com>
2025-01-14 11:22:21 +0100

    Add initial talk

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import cobalt2 from "./cobalt2.json";
+
+import mdx from "@astrojs/mdx";
+
+import expressiveCode from "astro-expressive-code";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [expressiveCode({ themes: [cobalt2] }), mdx()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
