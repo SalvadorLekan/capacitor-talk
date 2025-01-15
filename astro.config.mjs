7777452895eee4b36e9c4935d6883ebccd12c7bf
SalvadorLekan <lekansalvador@gmail.com>
2025-01-15 19:39:17 +0100

    Add reasons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
 import cobalt2 from "./cobalt2.json";
 
 import mdx from "@astrojs/mdx";
@@ -10,7 +9,4 @@ import expressiveCode from "astro-expressive-code";
 // https://astro.build/config
 export default defineConfig({
   integrations: [expressiveCode({ themes: [cobalt2] }), mdx()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
 });
